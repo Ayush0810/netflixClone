6c8b23d5707179223f42c582b450004e79bb30f3
ayush0810 <ayushksingh867@gmail.com>
2023-09-28 07:12:47 +0530

    basic padding change

--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -12,33 +12,33 @@ function Signin() {
             alt=""
           />
         </div>
-        <div className="box absolute top-20 backdrop-contrast-200 p-4 rounded-lg shadow-lg">
+        <div className="box absolute top-20  backdrop-contrast-200 p-8 rounded-lg shadow-lg">
           <form action="">
             <span className="text-white text-left text-3xl relative left-8 top-8">
               Sign In
             </span>
             <input
-              className="bg-gray-700 h-12 w-[80%] p-5 rounded-md relative top-14 left-9"
+              className="bg-gray-800 h-12 w-[80%] p-5 rounded-md relative top-14 left-9"
               type="email"
               placeholder="Email or Phone Number"
             />
             <input
-              className="bg-gray-700 h-12 w-[80%] p-5 rounded-md relative top-[5rem] left-9"
+              className="bg-gray-800 h-12 w-[80%] p-5 rounded-md relative top-[5rem] left-9"
               type="email"
               placeholder="Password"
             />
 
-            <button className="bg-red-800 contrast-125 h-14 w-[80%] p-5 rounded-md relative top-[8rem] left-9 text-center text-white ">
+            <button className="bg-red-800 contrast-125 h-14 w-[80%] p-5 rounded-md relative top-[7rem] left-9 text-center text-white ">
               Sign in
             </button>
-            <div className="mt-2 w-[80%] relative top-[8rem] left-9 flex justify-between ">
+            <div className="mt-2 w-[80%] relative top-[7rem] left-9 flex justify-between ">
               <span className="text-gray-700 text-sm">
                 <input className="bg-gray-700" type="checkbox" />Remember me
               </span>
               <h1 className="text-gray-700 text-sm">Need help?</h1>
             </div>
 
-            <div className="w-[80%] relative top-[15rem] left-9 space-y-4">
+            <div className="w-[80%] relative top-[13rem] left-9 space-y-4">
               <h1 className="text-white">
                 <span className="text-gray-700">New to Netflix?</span> Sign up
                 now.
